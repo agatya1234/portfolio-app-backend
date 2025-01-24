92365a6a842f150731b9cf762548a04e131cdad4
dev_ashishpal <agatyamishra@gmail.com>
2025-01-25 01:18:35 +0530

    change routes

--- controller/stockController.js
+++ controller/stockController.js
@@ -1,5 +1,6 @@
 import Stock from "../models/stockModel.js";
 
+// fetch all stocks and calculate portfolio value
 const getStocks = async (req, res) => {
   try {
     const stocks = await Stock.find();
@@ -9,5 +10,64 @@ const getStocks = async (req, res) => {
   }
 };
 
+// add a new stock
+const addStock = async (req, res) => {
+  try {
+    const { userId, name, ticker, quantity, buyPrice } = req.body;
+    if (!userId || !name || !ticker || !quantity || !buyPrice) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
 
-export { getStocks }
+    const stock = new Stock({ userId, name, ticker, quantity, buyPrice });
+    await stock.save();
+    res.status(201).json({ message: "Stock added successfully", data: stock })
+  } catch (error) {
+    res.status(500).json({ message: 'Error adding stock', error });
+  }
+}
+
+// Update existing stock details
+const updateStock = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { quantity } = req.body;
+
+    if (!quantity) {
+      return res.status(400).json({ message: 'Quantity is required' });
+    }
+
+    const stock = await Stock.findByIdAndUpdate(
+      id,
+      { quantity },
+      { new: true }
+    );
+
+    if (!stock) {
+      return res.status(404).json({ message: 'Stock not found' });
+    }
+
+    res.status(200).json({ message: 'Stock updated successfully', stock });
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating stock', error });
+  }
+}
+
+// remove stock
+const deleteStock = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const stock = await Stock.findByIdAndDelete(id);
+
+    if (!stock) {
+      return res.status(404).json({ message: 'Stock not found' });
+    }
+
+    res.status(200).json({ message: 'Stock deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting stock', error });
+  }
+}
+
+
+export { getStocks, addStock, updateStock, deleteStock }
--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { getStocks } from "../controller/stockController.js";
+import { addStock, getStocks } from "../controller/stockController.js";
 const router = express.Router();
 
 router.get('/', getStocks)
+router.post('/', addStock)
+router.put('/:id', addStock)
+router.delete('/:id', addStock)
 
 export default router;
